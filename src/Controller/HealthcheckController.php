b96b845c1ae5b1655924f9ffd9360f9c83912ce8
apolkova <120262575+apolkova@users.noreply.github.com>
2025-08-22 10:46:44 +0200

    healthcheckController
    aby se dalo overit, ze symfony v kontejneru bezi.

---
+++ src/Controller/HealthcheckController.php
@@ -0,0 +1,16 @@
+﻿<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HealthcheckController
+{
+    #[Route("/healthz", methods: ["GET"])]
+    public function __invoke(): Response
+    {
+        return new Response("ok", 200, ["Content-Type" => "text/plain"]);
+    }
+}
